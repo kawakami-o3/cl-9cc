76752a42c7d1b82a5fe0f26b81a2976a2ee0328e
kawakami <kawakami.ozone@gmail.com>
2018-11-16 04:11:12 +0900

    first commit

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+try() {
+  expected="$1"
+  input="$2"
+  ros compiler.ros "$input" > tmp.s
+  gcc -static -o tmp tmp.s
+  ./tmp
+  actual="$?"
+  if [ "$actual" != "$expected" ]; then
+    echo "$input expected, but got $actual"
+    exit 1
+  fi
+}
+try 0 0
+try 42 42
+echo OK
